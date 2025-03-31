80ba54733dd0c28b708492825df52d9747af0324
rubences <91731294+rubences@users.noreply.github.com>
2025-03-31 10:32:50 +0000

    casi lo tenemos

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     game.spawn_player("Player1")
     game.spawn_opponent(is_star=True)
     game.update_opponent()
-
-
+    game.update_player()
+    game.player.shoot()
+    game.opponent.shoot()
     game.update()
     game.end_game()
     game.reset()
